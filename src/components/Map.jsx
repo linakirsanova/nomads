263ce2d3ac4c2b7f681db66f44a7e8237e2c9e03
Angelina Filippova <linakirsanova@gmail.com>
2018-06-14 17:27:59 +0300

    Improve map sizing depending on the screen size

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -18,7 +18,7 @@ export default class Map extends React.Component {
 				isMarkerShown
 				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBN13XZ_q8-6d-P20Hln59RhqWxUgQo5W0&v=3.exp&libraries=geometry,drawing,places"
 				loadingElement={<div style={{ height: `100%` }} />}
-				containerElement={<div style={{ height: `600px` }} />}
+				containerElement={<div style={{ height: `100%`, width: `100%` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
 			/>
 		)
@@ -43,23 +43,23 @@ const GoogleMapComponent = withScriptjs(withGoogleMap((props) => {
 						{isOpen && <InfoWindow>
 							<div>
 								{`city: ${city}, users:`}
-									<ul style={{listStyleType: 'none', padding: 3, margin: 0 }}>
-										{
-											JSON.parse(user).map((el, i) => {
-												// (i + 1) % 3 === 0 || i === 0
-												return (
-													<li style={{ position: 'relative' }}>
-														<table>
-															<tr>
-																<td><img src={avatars[i]} height="25" width="25" alt="avatar"/></td>
-																<td style={{verticalAlign: 'middle'}}><span>{`${el}`}</span></td>
-															</tr>
-														</table>
-													</li>
-												)
-											})
-										}
-									</ul>
+								<ul style={{listStyleType: 'none', padding: 3, margin: 0 }}>
+									{
+										JSON.parse(user).map((el, i) => {
+											// (i + 1) % 3 === 0 || i === 0
+											return (
+												<li style={{ position: 'relative' }}>
+													<table>
+														<tr>
+															<td><img src={avatars[i]} height="25" width="25" alt="avatar"/></td>
+															<td style={{verticalAlign: 'middle'}}><span>{`${el}`}</span></td>
+														</tr>
+													</table>
+												</li>
+											)
+										})
+									}
+								</ul>
 							</div>
 						</InfoWindow>}
 					</Marker>
